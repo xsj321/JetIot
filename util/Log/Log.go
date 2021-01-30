4bdca5272a210d5dac31252074ff6e7b5edf894d
xsj321 <xsj08262334910>
2021-01-30 16:39:53 +0800

    1.完成了mqtt订阅事件回调框架的设计 2.实现了通过mqtt更改服务器中保存的数据 3.修改了Log包更加美观 4.修改了部分的底层逻辑

--- util/Log/Log.go
+++ util/Log/Log.go
@@ -3,7 +3,8 @@ package Log
 import (
 	"github.com/sirupsen/logrus"
 	"os"
-	"time"
+	"runtime"
+	"strconv"
 )
 
 var logger = logrus.New()
@@ -12,25 +13,37 @@ var Entry *logrus.Entry
 func init() {
 	logger.SetOutput(os.Stdout)
 	logger.SetLevel(logrus.DebugLevel)
-	logger.Formatter = &logrus.TextFormatter{}
+	logger.Formatter = &logrus.TextFormatter{
+		TimestampFormat:  "2006-01-02 15:04:05",
+		FullTimestamp:    true,
+		CallerPrettyfier: callerFormatter,
+	}
 	logger.SetReportCaller(true)
-	Entry = logger.WithFields(logrus.Fields{
-		"Time": time.Now(),
-	})
+	Entry = logger.WithFields(logrus.Fields{})
+}
+
+func callerFormatter(frame *runtime.Frame) (function string, file string) {
+	line := frame.Line
+	funcName := "[" + frame.Function + "]" + "[" + strconv.Itoa(line) + "]"
+	return funcName, ""
 }
 
 func D() func(args ...interface{}) {
+	logger.SetReportCaller(true)
 	return Entry.Debug
 }
 
 func W() func(args ...interface{}) {
+	logger.SetReportCaller(true)
 	return Entry.Warn
 }
 
 func E() func(args ...interface{}) {
+	logger.SetReportCaller(true)
 	return Entry.Error
 }
 
 func I() func(args ...interface{}) {
+	logger.SetReportCaller(false)
 	return Entry.Info
 }
